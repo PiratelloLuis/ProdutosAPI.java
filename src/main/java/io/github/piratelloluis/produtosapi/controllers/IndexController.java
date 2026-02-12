d85fbf78066d1b458dafddea74a0ce42a25199c6
andre <andre.piratello@hotmail.com>
2026-02-12 13:57:40 -0300

    fisrt commit

---
+++ src/main/java/io/github/piratelloluis/produtosapi/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package io.github.piratelloluis.produtosapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class IndexController {
+
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+
+}
